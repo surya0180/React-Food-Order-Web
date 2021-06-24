3d8893e011dc4d7eae30ace520b1da002f300cc4
surya0180 <teja8551@gmail.com>
2021-06-24 15:43:19 +0530

    Created header component completely

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from  "../../assets/meals.jpg";
+import HeaderCardButton from "./HeaderCardButton";
 
 const Header = () => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCardButton></HeaderCardButton>
       </header>
       <div className={classes['main-image']}>
         <img
---
+++ src/components/Layout/HeaderCardButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon'
+import classes from './HeaderCardButton.module.css'
+
+
+const HeaderCardButton = (props) => {
+    return <React.Fragment>
+        <button className={classes.button}>
+            <span className={classes.icon}><CartIcon/></span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>4</span>
+        </button>
+    </React.Fragment>
+};
+
+export default HeaderCardButton;
