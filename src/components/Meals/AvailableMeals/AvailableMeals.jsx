15bf9005d90736e1f573888f57e9894eae9d3131
surya0180 <teja8551@gmail.com>
2021-06-25 11:40:45 +0530

    Added meals list component

---
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import DUMMY_MEALS from "../dummy-meals";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "../MealItem/MealItem";
+import Card from "../../UI/Card";
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card><ul>{mealsList}</ul></Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './MealItem.module.css';
+const MealItem = (props) => {
+    return (
+        <li className={classes.meal} id={props.id}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{props.price}</div>
+            </div>
+            <div>
+                
+            </div>
+        </li>
+    )
+};
+
+export default MealItem;
